7e9780a4996078240e0d5df54b9be323c60ef190
testerprof <sepitamazand@gmail.com>
2022-03-06 22:36:54 +0330

    create Counter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ counter , setCounter ] = useState<number>(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-      <h1>Hello Typescript</h1>
+      <h1>Counter : {counter}</h1>
+      <button onClick={increaseCounter}>Increase Counter</button>
     </div>
   );
 }
